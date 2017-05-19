9f09ce41b643fe2dfb0bd298f87bc6b3bc403154
itthinx <itthinx@gmail.com>
2017-05-19 19:59:04 +0200

    finished

--- groups-buddpress-compatibility.php
+++ groups-buddpress-compatibility.php
@@ -14,19 +14,18 @@ if ( !defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Identifies a call on the groups_created_group action from Groups (as opposed to those calls
+ * coming from BuddyPress). Disables the related BuddyPress actions when appropriate, i.e. when
+ * the call comes from Groups and not from BuddyPress.
+ *
+ * @param int $group_id
+ */
 function groups_buddpress_compatibility_groups_created_group_pre( $group_id ) {
 
 	$is_groups = false;
 
-// 	$args = func_get_args();
-// 	if ( count( $args ) < 2 ) {
-// 		$is_groups = true;
-// 	}
-
-// 	error_log(__FUNCTION__ . ' ' . var_export($args,true)); // @todo remove
-
 	$traces = debug_backtrace();
-	//error_log( __FUNCTION__. ' trace ... ' . var_export($traces,true) ); // @todo remove
 	if ( is_array( $traces ) ) {
 		foreach( $traces as $trace ) {
 			if ( isset( $trace['file'] ) ) {
@@ -38,7 +37,7 @@ function groups_buddpress_compatibility_groups_created_group_pre( $group_id ) {
 		}
 	}
 	if ( $is_groups ) {
-		error_log('call is assumed from groups - removing BP actions'); // @todo remove
+		// call is assumed to come from groups - removing BP actions
 		global $groups_buddpress_compatibility;
 		$groups_buddpress_compatibility = true;
 		remove_action( 'groups_created_group', 'groups_update_last_activity' );
@@ -47,10 +46,15 @@ function groups_buddpress_compatibility_groups_created_group_pre( $group_id ) {
 	}
 }
 
+/**
+ * Adds BuddyPress actions hooked on groups_created_group back.
+ *
+ * @param int $group_id
+ */
 function groups_buddpress_compatibility_groups_created_group_post( $group_id ) {
 	global $groups_buddpress_compatibility;
 	if ( isset( $groups_buddpress_compatibility ) ) {
-		error_log('call is assumed from groups - adding BP actions'); // @todo remove
+		// call was assumed to come from groups - adding BP actions back
 		add_action( 'groups_created_group', 'groups_update_last_activity' );
 		add_action( 'groups_created_group', 'bp_groups_clear_group_creator_cache', 10, 2 );
 		add_action( 'groups_created_group', 'bp_core_clear_cache' );
